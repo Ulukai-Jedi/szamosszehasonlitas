b14499b352e7f288cc6e265fa4cb262cf2f4d146
Gergely Brunda <gergelybrunda@gmail.com>
2021-08-25 23:17:17 +0200

    előző verzióban elfelejtettem else ágat, így ha a feltétel ellenkező módon teljesült a program kilépett helyes eredmény nélkül, most már van jó else ág

--- szamosszehasonlitas/main.cpp
+++ szamosszehasonlitas/main.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 
 int main() {
-	int szam1 = 0;
+	int szam1 = 0; //deklaráljuk a két változónkat
 	int szam2 = 0;
 
-	std::cout << "Kerem adjon meg ket szamot" << std::endl;
+	std::cout << "Kerem adjon meg ket szamot" << std::endl; //meg kell mondani a User-nek, hogy mit akar tõle a program
 
-	std::cin >> szam1;
+	std::cin >> szam1; //A user szépen beadja a két számot
 	std::cin >> szam2;
 
-	if (szam1 == szam2) {
+	if (szam1 == szam2) { //Itt csinálunk egy teszt sort, hogy történjen valami, ha netán egyenlõ!
 		std::cout << "A ket szam egyenlo, kerem adjon meg uj szamokat" << std::endl;
 		std::cin >> szam1;
 		std::cin >> szam2;
 	}
 	
-	if (szam1 > szam2) {
-		std::cout << "Az elso szam nagyobb, mint a masodik " << szam1 << ">" << szam2 << std::endl;
+	if (szam1 > szam2) { //Elsõ feltétel
+		std::cout << "Az elso szam nagyobb, mint a masodik " << szam1 << " > " << szam2 << std::endl;
+	}
+	else { //Második, "egyébként" feltétel
+		std::cout << "A masodik szam nagyobb, mint az elso " << szam2 << " > " << szam1 << std::endl;
 	}
 	return 0;
 }
